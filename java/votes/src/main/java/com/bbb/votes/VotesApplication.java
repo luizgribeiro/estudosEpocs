b345dcf74bf6c2815f3b178bb71940c0a36b805c
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2023-02-12 12:15:35 -0300

    update votes project

--- java/votes/src/main/java/com/bbb/votes/VotesApplication.java
+++ java/votes/src/main/java/com/bbb/votes/VotesApplication.java
@@ -1,13 +1,17 @@
 package com.bbb.votes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VotesApplication {
+	@Value("server.port")
+	static String teste;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VotesApplication.class, args);
 	}
+
 
 }
--- java/votes/src/main/java/com/bbb/votes/controllers/VoteController.java
+++ java/votes/src/main/java/com/bbb/votes/controllers/VoteController.java
@@ -1,2 +1,43 @@
-package com.bbb.votes.controllers;public class VoteController {
+package com.bbb.votes.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+class Vote {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+@RequestMapping("/vote")
+@RestController
+public class VoteController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    @PostMapping
+    String createVote(@RequestBody Vote vote) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println("Fui testado");
+        String serializedVote;
+        try {
+            serializedVote = objectMapper.writeValueAsString(vote);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        kafkaTemplate.send("votes", serializedVote);
+        return "oi";
+    }
 }
